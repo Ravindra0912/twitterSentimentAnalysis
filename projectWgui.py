dbfa977d077466a363e4d04be33404f8270ff3ba
ravindra0912 <ravindra.shrivastava.9@gmail.com>
2017-04-27 21:08:15 +0530

    First COmmit

--- projectWgui.py
+++ projectWgui.py
@@ -1,8 +1,6 @@
 import tweepy
-import password
 import json
 from tweepy.streaming import StreamListener
-from tweepy import streaming
 from tweepy.streaming import Stream
 from tweepy import OAuthHandler
 from textblob import TextBlob
